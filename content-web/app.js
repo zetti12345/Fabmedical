c51712876bdcfd8c8f39966362618e061356ad04
zetti12345 <peter.gassner@outlook.com>
2022-12-01 12:53:32 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("450fa392-4bfd-4430-9068-3292490c62d8");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
